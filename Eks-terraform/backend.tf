276bfdf4e045ecace20aa0500d59814910692d27
prashanthnaid <168257081+prashanthnaid@users.noreply.github.com>
2024-10-17 11:06:31 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "redd1" # Replace with your actual S3 bucket name
+    bucket = "dotnet143" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-northeast-3"
+    region = "ap-south-1"
   }
 }
